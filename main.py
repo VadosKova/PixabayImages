566b6fec69dbcd6658a04f41ed56f9580a2cfdce
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-23 13:16:40 +0300

    Added filter

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import random
 
 API_KEY = "49887797-4504305467ee5706dfdb7b3be"
 
-def update_image():
-    url = f"https://pixabay.com/api/?key={API_KEY}&image_type=photo"
+categories = ["All", "Nature", "Car", "Business", "Forest", "Wolf"]
+
+def update_image(category):
+    query = category if category != "Все" else ""
+
+    url = f"https://pixabay.com/api/?key={API_KEY}&q={query}&image_type=photo"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -23,6 +27,8 @@ def update_image():
             label.config(image=tk_img)
             label.image = tk_img
 
+def change_category(selected_category):
+    update_image(selected_category)
 
 root = Tk()
 root.title("Pixabay Images")
@@ -31,7 +37,13 @@ root.geometry("500x500")
 label = Label(root)
 label.pack(padx=20, pady=20)
 
-Button(root, text="Next", command=update_image).pack(pady=10)
-update_image()
+category_var = StringVar(root)
+category_var.set(categories[0])
+
+category_menu = OptionMenu(root, category_var, *categories, command=change_category)
+category_menu.pack(padx=20, pady=10, side=RIGHT)
+
+Button(root, text="Next", command=lambda: update_image(category_var.get())).pack(pady=10)
+update_image(category_var.get())
 
 root.mainloop()
