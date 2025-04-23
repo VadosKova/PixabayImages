6447213b6479f35b1becca4d6198ff81bfe5541c
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-23 12:53:12 +0300

    Added components for window

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ from PIL import Image, ImageTk
 import random
 
 API_KEY = "49887797-4504305467ee5706dfdb7b3be"
+
+
+
+
+root = Tk()
+root.title("Pixabay Images")
+root.geometry("500x500")
+
+label = Label(root)
+label.pack(padx=20, pady=20)
+
+Button(root, text="Next").pack(pady=10)
+
+
+root.mainloop()
