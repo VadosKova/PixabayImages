efaf22e8773f7f9a7981dcac52b8e4a4637be0e0
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-23 13:04:10 +0300

    Showing random image

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ API_KEY = "49887797-4504305467ee5706dfdb7b3be"
 def update_image():
     url = f"https://pixabay.com/api/?key={API_KEY}&image_type=photo"
     response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data["hits"]:
+            img_url = random.choice(data["hits"])["largeImageURL"]
+            img_data = requests.get(img_url).content
+            ext = img_url.split('.')[-1]
 
 
 root = Tk()
